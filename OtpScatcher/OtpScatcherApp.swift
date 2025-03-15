89363e153f80dc499b81a1596435e31565cd0522
Arumugam J <sixface@msn.com>
2025-03-15 03:46:42 -0700

    Initial Commit

---
+++ OtpScatcher/OtpScatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtpScatcherApp.swift
+//  OtpScatcher
+//
+//  Created by Arumugam Jeganathan on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct OtpScatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
