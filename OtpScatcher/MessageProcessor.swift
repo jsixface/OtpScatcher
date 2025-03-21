ee8eaf93ea0442faca042c1cad117d7786056bcf
Arumugam J <sixface@msn.com>
2025-03-20 23:59:05 -0700

    Adding check to break out of shm read loop.

--- OtpScatcher/MessageProcessor.swift
+++ OtpScatcher/MessageProcessor.swift
@@ -20,6 +20,7 @@ class MessageProcessor {
     let textColumn = SQLite.Expression<String?>("text")
     let idColumn = SQLite.Expression<Int64>("ROWID")
     var dbFileActual: String
+    var lastIdRead: Int64 = 0
 
     init(dbFile: String? = nil) throws {
         dbFileActual = dbFile ?? "\(NSHomeDirectory())/Library/Messages/chat.db"
@@ -32,7 +33,7 @@ class MessageProcessor {
                 for: ObjectIdentifier(self),
                 with: { event in
                     print("Got notified for file: \(event.path)")
-                    if event.path.hasSuffix(".db") || event.path.hasSuffix(".db-shm") {
+                    if event.path.hasSuffix(".db") || event.path.hasSuffix(".db-wal") {
                         withHandler()
                     }
                 })
@@ -47,16 +48,24 @@ class MessageProcessor {
 
     func getLatestText() -> SmsCode? {
         do {
+            print ("Last row id = \(lastIdRead)")
             let db = try Connection(dbFileActual, readonly: true)
             let query = messages.select(attributedBodyColumn, textColumn, idColumn)
-                .order(idColumn.desc).limit(1)
-            let codesScanned = try db.prepare(query).map { row in
-                row[attributedBodyColumn]?.asMessageContent()
+                .filter(idColumn > lastIdRead)
+                .order(idColumn.desc)
+                .limit(1)
+            let result = try db.prepare(query).map { row in
+                (row[idColumn], row[attributedBodyColumn])
             }
-            if !codesScanned.isEmpty { return codesScanned[0] }
+            if !result.isEmpty {
+                let (rowId, blob) = result[0]
+                lastIdRead = rowId
+                if let smsCode = blob?.asMessageContent() {
+                    if smsCode.codePresent { return smsCode }
+                }
+            }
         } catch {
-            logger.error(
-                "Error while talking to database. [\(self.dbFileActual)]")
+            logger.error("Error while talking to database. [\(self.dbFileActual)]")
         }
         return nil
     }
